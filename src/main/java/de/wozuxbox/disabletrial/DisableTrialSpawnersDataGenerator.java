7c46f4a9d0f07cddb90c6e34a42dfe02c7784a55
WozuXbox <WozuXbox@proton.me>
2026-02-15 18:00:44 +0100

    added mod template stuff and deleted neoforge mod

---
+++ src/main/java/de/wozuxbox/disabletrial/DisableTrialSpawnersDataGenerator.java
@@ -0,0 +1,11 @@
+package de.wozuxbox.disabletrial;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DisableTrialSpawnersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
