e4e426d6605b950dcb233460586acdb47769eb5c
WozuXbox <WozuXbox@proton.me>
2026-02-16 19:49:15 +0100

    tried to add Items

--- src/main/java/de/wozuxbox/disabletrial/ModItems.java
+++ src/main/java/de/wozuxbox/disabletrial/ModItems.java
@@ -1,9 +1,12 @@
 package  de.wozuxbox.disabletrial;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.component.Component;
 
 public class ModItems {
 	public static Item register(Item item, String id) {
@@ -16,9 +19,20 @@ public class ModItems {
 		// Return the registered item!
 		return registeredItem;
 	}
-	public static final Item ITEMA = register(
+	public static final Item COPPER_SPAWNER_KEY = register(
 		new Item(new Item.Settings()),
-		"itema"
+		"copper_spawner_key"
 	);
-	public static void initialize() {}
+	public static final Item DIAMOND_SPAWNER_KEY = register(
+		new Item(new Item.Settings()),
+		"diamond_spawner_key"
+	);
+	public static final Item NETHERITE_SPAWNER_KEY = register(
+		new Item(new Item.Settings()),
+		"netherite_spawner_key"
+	);
+	public static void initialize() {
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
+		.register((itemGroup) -> itemGroup.add(ModItems.COPPER_SPAWNER_KEY));
+	}
 }
